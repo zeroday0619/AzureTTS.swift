5ec6746e30b01dd570c0f7b20167102a05b078e2
zeroday0619 <zeroday0619_dev@outlook.com>
2022-08-11 20:58:55 +0900

    chore: project setup

---
+++ Sources/AzureTTS.swift/AzureTTS_swift.swift
@@ -0,0 +1,6 @@
+public struct AzureTTS_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AzureTTS.swiftTests/AzureTTS_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AzureTTS_swift
+
+final class AzureTTS_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AzureTTS_swift().text, "Hello, World!")
+    }
+}
